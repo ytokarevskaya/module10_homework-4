b7287d7f03cd33d45f528dad68fda23d6beace67
ytokarevskaya <lan2214@yandex.ru>
2020-07-08 23:09:39 +0300

    Проверено

--- 3.js
+++ 3.js
@@ -8,3 +8,7 @@ for (let i=value.length-1; i>=0; i--) {
 }
 
 console.log(reverseString);
+
+// Всё верно, но есть более короткий и компактный варинат решения:
+
+let reverseString = value.split("").reverse().join("");
--- 4.js
+++ 4.js
@@ -2,3 +2,8 @@
 
 randomValue = Math.round(Math.random() * 100);
 console.log(randomValue);
+
+// Не совсем верно, т.к. код выше будет возвращать рандомные числа в диапазоне от 0 до 99 (100 включено не будет). Верный вариант:
+
+const random = Math.floor(Math.random() * 101);
+console.log(random);
